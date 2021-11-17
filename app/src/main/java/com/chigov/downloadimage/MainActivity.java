63724b8dd462893c67e3b4d3797c81f624c99504
Max2 <chigov@gmail.com>
2021-11-17 21:09:41 +0200

    Initial commit

---
+++ app/src/main/java/com/chigov/downloadimage/MainActivity.java
@@ -0,0 +1,20 @@
+package com.chigov.downloadimage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickDownLoadImage(View view) {
+    }
+}
