f08bedf2029679cffe6cd8eb89e54ad4ca2d1c69
chigov2 <chigov@gmail.com>
2021-11-18 12:30:14 +0200

    DownLoadImage-final

--- app/src/main/java/com/chigov/downloadimage/MainActivity.java
+++ app/src/main/java/com/chigov/downloadimage/MainActivity.java
@@ -2,19 +2,74 @@ package com.chigov.downloadimage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
 public class MainActivity extends AppCompatActivity {
     private ImageView imageView;
+    private String url = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
     }
 
     public void onClickDownLoadImage(View view) {
+        DownloadImageTask task = new DownloadImageTask();
+        Bitmap bitmap = null;
+        try {
+            bitmap = task.execute(url).get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        imageView.setImageBitmap(bitmap);
+
+    }
+
+    private static class DownloadImageTask extends AsyncTask<String, Void, Bitmap>{
+
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+
+            HttpURLConnection urlConnection = null;
+            //StringBuilder result = new StringBuilder();
+            //url = new URL(strings[0]);
+            try {
+                URL url = new URL(strings[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();//соединение открыто
+                //создаем поток ввода
+                InputStream inputStream = urlConnection.getInputStream();
+                //надо знать
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                return bitmap;
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (urlConnection != null){
+                    urlConnection.disconnect();
+                }
+            }
+
+
+            return null;
+        }
     }
 }
